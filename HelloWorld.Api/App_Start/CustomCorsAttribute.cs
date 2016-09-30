ea2dc4c41f51190582aded0fdf27efb45b83fa87
Joshua Blackstone <joshuablackstone@hotmail.com>
2016-09-30 10:56:58 -0400

    Api Project

---
+++ HelloWorld.Api/App_Start/CustomCorsAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Cors;
+using System.Web.Http.Cors;
+
+namespace HelloWorld.Api
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class CustomCorsAttribute : Attribute, ICorsPolicyProvider
+    {
+        private readonly CorsPolicy _policy;
+        private readonly string OriginAllowed = "http://localhost:52653";
+
+        public CustomCorsAttribute()
+        {
+            // Create a CORS policy.
+            _policy = new CorsPolicy
+            {
+                AllowAnyMethod = true,
+                AllowAnyHeader = true,
+                AllowAnyOrigin = false,
+                ExposedHeaders = { "Content-Disposition" }
+            };
+
+            _policy.Origins.Add(OriginAllowed);
+        }
+
+        public async Task<CorsPolicy> GetCorsPolicyAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(_policy);
+        }
+    }
+}
---
+++ HelloWorld.Api/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using HelloWorld.Data;
+using System.Web.Http;
+
+namespace HelloWorld.Api.Controllers
+{
+    public class HelloWorldController : ApiController
+    {
+        IDataRepository data;
+
+        public HelloWorldController(IDataRepository _data)
+        {
+            data = _data;
+        }
+
+        public string Get()
+        {
+            return data.GetValue();
+        }
+    }
+}
