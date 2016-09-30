38cf70ba1ae016d0c50a444fadeab7e16a994924
Joshua Blackstone <joshuablackstone@hotmail.com>
2016-09-30 10:58:51 -0400

    Front End Angular Project

---
+++ HelloWorld.Web/app/controllers/helloWorldController.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('helloworld');
+
+    app.controller('HelloWorldCtlr', ['HelloWorldService', function(HelloWorldService){
+        var vm = this;
+        vm.value = "-- Loading --";
+        HelloWorldService.get().then(function (data) {
+            vm.value = data.data;
+        });
+    }]);
+})();
---
+++ HelloWorld.Web/app/services/helloWorldService.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    var serviceId = 'HelloWorldService';
+    var app = angular.module('helloworld');
+    app.constant('HelloWorldServiceUrl', 'http://localhost:63403/api/helloworld');
+    app.factory(serviceId, ['$http', 'HelloWorldServiceUrl',
+        function ($http, serviceUrl) {
+            return {
+                get: function () {
+                    return $http.get(serviceUrl);
+                }
+            };
+        }]);
+})();
